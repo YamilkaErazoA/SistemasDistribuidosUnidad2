1cedc9a388c894d19cb1da13967ea2e2910bd8c7
YamilkaErazoA <76630102+YamilkaErazoA@users.noreply.github.com>
2022-06-24 12:23:30 -0500

    Hilo Concurrente

--- hilo3_posix.cpp
+++ hilo3_posix.cpp
@@ -9,6 +9,9 @@
 // Vector de identificadores de hilos
 pthread_t tid[MAX_THREADS]; // vector de tipo hilo, almacenar los identificadores de todos los hilos q vamos creando
 
+// definición de un candado para arbitrar el acceso a una variable compartida
+pthread_mutex_t mutex;
+
 // variables globales
 int *array; // un apuntador a un vecto de enteros
 int length = VECTOR_SIZE; // máxima longitud del vector
@@ -24,13 +27,17 @@ void *count3s_thread(void *arg){
 	int length_per_thread = length / max_threads; // cada hilo procese una parte del vector,
 	long id = (long)arg;
 	int start = id * length_per_thread;
+	
+	pthread_mutex_lock(&mutex); // se llama esta sección crítica
+	
 	for(i = start; i < start + length_per_thread; i++){
 		if (array[i] == 3){
 			count++;
 		}
 	}
-	
-	pthread_exit(NULL);
+	pthread_mutex_unlock(&mutex);
+	return NULL;
+	//pthread_exit(NULL);
 }
 
 //inicializador del vector
@@ -73,13 +80,17 @@ int main(int argc, char* argv[]){
 	printf("Vector initialized! \n");
 	fflush(stdout);
 	t1 = clock();
+	
+	//El candado que evita la concurrencia
+	pthread_mutex_init(&mutex, NULL);
+	
 	// comienza el procesamiento
 	while(i < max_threads){
 		err = pthread_create(&tid[i], NULL, &count3s_thread, (void*)i);
 		if (err != 0)
 			printf("[3s-01] Can't create a thread [%ld]\n",i);
 		else
-			printf("[3s-01] Thread created [%ld]\n",i);
+			printf("[3s-01] Thread created [%ld]\n", i);
 		i++;
 	}
 	i = 0;
@@ -97,6 +108,7 @@ int main(int argc, char* argv[]){
 	t2 = clock();
 	printf("[3s-01] Count by threads %d\n",count);
 	printf("[3s-01] Double check %d!\n",double_count);
+	pthread_mutex_destroy(&mutex);
 	printf("[3s-01] Elapsed time %f\n",(((float)t2 - (float)t1) /1000000.0F) * 1000);
 	printf("Finishing 3s-00");
 	return 0;
