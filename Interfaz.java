f4ccad568edb931937e13135681b1d046590a5c0
YamilkaErazoA <76630102+YamilkaErazoA@users.noreply.github.com>
2022-06-24 13:17:31 -0500

    Delete Interfaz.java

--- Interfaz.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-
-public interface Interfaz extends Remote{
-    public String retiro (Float dinero) throws RemoteException;
-    public String deposito(Float dinero) throws RemoteException;
-}
